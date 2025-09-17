f38f2502adf96ecb06d6890057f11dd512d340c4
Gero2208 <ge.ortiz@alumno.um.edu.ar>
2025-09-17 02:14:50 -0300

    Cambios en CLI

--- backgammon/cli/cli.py
+++ backgammon/cli/cli.py
@@ -1,16 +1,23 @@
-from core.board import Board
+from ..core.player import Player
+from ..core.board import Board
 
 def main():
     print("Iniciando Backgammon")
 
-    # Crear tablero y jugadores
+    nombre1 = input("Nombre del Jugador 1: ")
+    color = input("Color del Jugador 1 (Blanco/Negro): ").lower()
+    while color not in ("blanco", "negro"):
+        print("Color inválido. Debe ser 'Blanco' o 'Negro'")
+        color = input("Color del Jugador 1 (Blanco/Negro): ").lower()
+    nombre2 = input("Nombre del Jugador 2: ")
+
+    p1 = Player(color, nombre1, +1)
+    p2 = Player("", nombre2, -1)
+    p2.asignar_color_opuesto(color)
+
+    print("Tablero:")
     board = Board()
     board.setup()
-
-    # Crear dos jugadores (id, color, direction)
-    # Convención: 'jugador1' mueve +1, 'jugador2' mueve -1 (ajusta según tu tablero)
-    p1 = Player("jugador1", "blanco", +1)
-    p2 = Player("jugador2", "negro", -1)
 
 
 if __name__ == '__main__':
