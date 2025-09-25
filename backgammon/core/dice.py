bc2b590de865ef880e1ea451483a4bb75460d464
Gero2208 <ge.ortiz@alumno.um.edu.ar>
2025-09-25 18:52:01 -0300

    Implementacion atributos y metodo roll de la clase Dice

--- backgammon/core/dice.py
+++ backgammon/core/dice.py
@@ -1,3 +1,10 @@
+import random
+
 class Dice:
+
+    def __init__(self):
+        self.__valor__ = [0, 0]
+
     def roll(self):
-        pass
+        self.__valor__ = [random.radint(1, 6), random.radint(1, 6)]
+        return self.__valor__
