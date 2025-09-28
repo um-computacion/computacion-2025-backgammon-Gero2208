9493060be75ad868a44b3e635ade470a1e267e7a
Gero2208 <ge.ortiz@alumno.um.edu.ar>
2025-09-28 19:45:25 -0300

    Implementacion metodo dobles para Dice y correccion de errores

--- backgammon/core/dice.py
+++ backgammon/core/dice.py
@@ -6,5 +6,8 @@ class Dice:
         self.__valor__ = [0, 0]
 
     def roll(self):
-        self.__valor__ = [random.radint(1, 6), random.radint(1, 6)]
+        self.__valor__ = [random.randint(1, 6), random.randint(1, 6)]
         return self.__valor__
+    
+    def dobles(self):
+        return self.__valor__ [0] == self.__valor__ [1]
